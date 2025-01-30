5cf40af8028a09a562381dd4be4087065b4459be
Neha Sapkal <sapkalneha90@gmail.com>
2025-01-31 00:14:11 +0530

    Add files via upload

--- Leap_year_or_not.java
+++ Leap_year_or_not.java
@@ -3,7 +3,13 @@ package Control_satement;
 public class Leap_year_or_not {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int year = 2000;
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+			System.out.println("Leap year");
+		} else {
+			System.out.println("Not leap year");
+		}
 
 	}
 
